8c80baebad54cb9aea51de7762644b022b5818bb
ShadowRaccoon <shadowRaccoon@users.noreply.github.com>
2018-06-18 23:39:37 -0300

    Agrego Javadoc

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,7 +1,19 @@
 package loom.hola_mundo;
 
+/**
+ * @author ShadowRaccoon
+ *
+ */
 public class HolaMundo {
 
+    /**
+     * Saluda a la persona.
+     * Este método recibe por parametro el nombre de la parsona
+     * y devuelve un String saludando por su nombre.
+     * 
+     * @param persona a saludar.
+     * @return persona saludada.
+     */
     public String saludar(final String persona) {
         return "¡Hola, " + persona + "!";
     }
