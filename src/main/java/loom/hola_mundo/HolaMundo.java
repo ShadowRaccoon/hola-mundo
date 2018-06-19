616123e59448120213297d85db066de888d6b841
ShadowRaccoon <shadowRaccoon@users.noreply.github.com>
2018-06-19 20:42:23 -0300

    fix trailing spaces

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -10,7 +10,7 @@ public class HolaMundo {
      * Saluda a la persona.
      * Este método recibe por parametro el nombre de la parsona
      * y devuelve un String saludando por su nombre.
-     * 
+     *
      * @param persona a saludar.
      * @return persona saludada.
      */
